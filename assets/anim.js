606068e777a00daf70cb36ba642596c88d43c976
ReiiAp <raihanalena@gmail.com>
2026-02-03 20:48:45 +0700

    1

--- assets/anim.js
+++ assets/anim.js
@@ -1,9 +1,7 @@
-// Loader
 window.onload = () => {
   document.getElementById("loader").style.display="none";
 }
 
-// Dark/Light toggle
 function toggleMode(){
   document.body.classList.toggle("dark");
 }
