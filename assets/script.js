f0f49cfbecac8220780c2d5dc7bf70094d1ca871
ReiiAp <raihanalena@gmail.com>
2026-02-03 21:09:07 +0700

    1

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,20 @@
 window.onload = () => {
   document.getElementById("loader").style.display="none";
+  reveal();
 }
 
 function toggleMode(){
   document.body.classList.toggle("dark");
+}
+
+function reveal(){
+  let items = document.querySelectorAll(".reveal");
+  window.addEventListener("scroll", () => {
+    items.forEach(el => {
+      let top = el.getBoundingClientRect().top;
+      if(top < window.innerHeight - 100){
+        el.classList.add("active");
+      }
+    });
+  });
 }
